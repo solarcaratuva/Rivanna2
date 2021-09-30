fa874f4cc910ee1872059f7e48e3bb7d8345d688
John Chrosniak <chrosniakj@gmail.com>
2021-09-29 20:24:45 -0400

    added main loop testing for motor interface

--- Motor/src/main.cpp
+++ Motor/src/main.cpp
@@ -1,6 +1,7 @@
 #include <mbed.h>
 #include "pindef.h"
 #include "Printing.h"
+#include "MotorInterface.h"
 
 #define TESTING     // only defined if using test functions
 // #define DEBUGGING   // only define if debugging
@@ -13,18 +14,27 @@ CAN can(CAN_RX, CAN_TX);
 DigitalOut canStby(CAN_STBY);
 Ticker canTxTicker;
 
+I2C throttle(PB_8, PB_9);//(SDA_ACCEL, SCL_ACCEL);
+I2C regen(PF_15, PF_14);//(SDA_REGEN, SCL_REGEN);
+I2C testMessage(PB_11, PB_10);
+DigitalIn testEn(PF_13);
+DigitalOut enable(PE_9);//(DIR_ACCEL);
+MotorInterface motorInterface(throttle, regen, enable);
+
 int main() {
     // device.set_baud(38400);
     
 #ifdef TESTING
     PRINT("start main() \r\n");
 #endif //TESTING
-    
+    // motorInterface.startTransmission();
     while(1){
         #ifdef TESTING
             PRINT("main thread loop \r\n");
         #endif //TESTING
-
-        thread_sleep_for(MAIN_LOOP_PERIOD_MS);
+        // printf("%d", testEn.read());
+        // motorInterface.sendThrottle(15);
+        // printf("%d", testMessage.read(1));
+        // thread_sleep_for(MAIN_LOOP_PERIOD_MS);
     }
 }
