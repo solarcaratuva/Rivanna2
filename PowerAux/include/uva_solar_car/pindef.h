0ec09ea0dfd577d57d888ab9d815260b9e5b82ac
Emmitt James <emmittjames@d-172-25-147-90.dhcp.virginia.edu>
2021-10-03 17:20:29 -0400

    add pindefs for poweraux

--- PowerAux/include/uva_solar_car/pindef.h
+++ PowerAux/include/uva_solar_car/pindef.h
@@ -7,9 +7,6 @@
 #define CAN_RX      PA_11   // Rx for MCU
 #define CAN_TX      PA_12
 #define CAN_STBY    PA_10   // Digital Output
-#define CAN2_RX     PB_12   // Rx for MCU
-#define CAN2_TX     PB_13
-#define CAN2_STBY   PB_9    // Digital Output
 
 /***************************/
 /* pin definitions for USB */
@@ -36,5 +33,27 @@
 /* pin definitions for Unused I/O */
 /**********************************/
 #define UNUSED_PIN_0    PA_2
+
+
+//Bottom
+#define HORN_EN               PA_5
+#define BMS_STROBE_CURRENT    PA_6
+#define BMS_STROBE_EN         PA_7
+#define BRAKE_LIGHT_CURRENT   PB_0
+#define BRAKE_LIGHT_EN        PB_1
+#define RIGHT_TURN_CURRENT    PB_2
+#define RIGHT_TURN_EN         PB_10
+
+//Top
+#define VDD                   VDD_3
+#define MAIN_CAN_STBY         PB_9
+#define USB_RX                PB_7
+#define USB_TX                PB_6
+#define BMS_CAN2_STBY         PB_5
+#define BMS_CAN2_TX           PB_4
+#define SWO                   PB_3
+#define JTDI                  PA_15
+#define SWCLK                 PA_14
+#define SWDIO                 PA_13
 
 #endif // PINDEF_H
