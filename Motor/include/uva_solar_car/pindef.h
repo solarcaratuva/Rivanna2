b1a09391073f64584950cf7bbe28cdf9bfdf4367
Ethanomicz <ett6xuq@virginia.edu>
2021-10-03 17:17:43 -0400

    Adding pin definitions for motor

--- Motor/include/uva_solar_car/pindef.h
+++ Motor/include/uva_solar_car/pindef.h
@@ -34,4 +34,57 @@
 /**********************************/
 #define UNUSED_PIN_0    PA_2
 
+/**********************************/
+/* pin definitions for Direction Acceleration */
+/**********************************/
+#define DIR_ACCEL   PC13
+
+// pin definition for NRST
+#define NRST    PG10-NRST
+
+// pin definition for MAIN_SWITCH
+#define MAIN_SWITCH PA2
+
+// pin definition for PWR_ECO
+#define PWR_ECO PA3
+
+// pin definition for FWD_REV
+#define FWD_REV PA4
+
+// pin definition for VDD
+#define VDD    VDD_3
+
+// pin definition for SDA_ACCEL
+#define SDA_ACCEL   PB9
+
+// pin definition for SWD
+#define SWD    PB3
+
+// pin definiiton SCL_ACCEL
+#define SCL_ACCEL   PA15
+
+// pin definition SWCLK
+#define SWCLK   PA14
+
+// pin defintion SWD10
+#define SWD10   PA13
+
+// pin definition for EXTRA_4
+#define EXTRA_4    PB5
+
+// pin definition for EXTRA_3
+#define EXTRA_3    PB4
+
+// pin definition for SWD
+#define SWD    PB3
+
+// pin definiiton SCL_ACCEL
+#define SCL_ACCEL   PA15
+
+// pin definition SWCLK
+#define SWCLK   PA14
+
+// pin defintion SWD10
+#define SWD10   PA13
+
 #endif // PINDEF_H
