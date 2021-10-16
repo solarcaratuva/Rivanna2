559a5fe74487acb5acc3b4311ae2584b55d9277e
John Chrosniak <jlc9wr@virginia.edu>
2021-10-16 15:23:43 -0400

    added code for testing interface

--- Motor/src/main.cpp
+++ Motor/src/main.cpp
@@ -8,33 +8,31 @@
 
 #define MAIN_LOOP_PERIOD_MS 1000  // units of 1 ms
 
+Mutex main_printing_mutex;
 BufferedSerial device(USBTX, USBRX);
 
 CAN can(CAN_RX, CAN_TX);
 DigitalOut canStby(CAN_STBY);
 Ticker canTxTicker;
 
-I2C throttle(PB_8, PB_9);//(SDA_ACCEL, SCL_ACCEL);
-I2C regen(PF_15, PF_14);//(SDA_REGEN, SCL_REGEN);
-I2C testMessage(PB_11, PB_10);
-DigitalIn testEn(PF_13);
-DigitalOut enable(PE_9);//(DIR_ACCEL);
+I2C throttle(SDA_ACCEL, SCL_ACCEL);
+I2C regen(SDA_REGEN, SCL_REGEN);
+DigitalOut enable(DIR_ACCEL);
 MotorInterface motorInterface(throttle, regen, enable);
 
 int main() {
     // device.set_baud(38400);
-    
+    init_printing(&main_printing_mutex);
 #ifdef TESTING
     PRINT("start main() \r\n");
 #endif //TESTING
-    // motorInterface.startTransmission();
+    motorInterface.startTransmission();
+    throttle.start();
     while(1){
         #ifdef TESTING
             PRINT("main thread loop \r\n");
         #endif //TESTING
-        // printf("%d", testEn.read());
-        // motorInterface.sendThrottle(15);
-        // printf("%d", testMessage.read(1));
-        // thread_sleep_for(MAIN_LOOP_PERIOD_MS);
+        motorInterface.sendThrottle(15);
+        thread_sleep_for(MAIN_LOOP_PERIOD_MS);
     }
 }
