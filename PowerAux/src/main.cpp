c4328fd33cd9ef968e38a1a9e60b93b0b7101e5c
Shreepa Parthaje <shreepa.parthaje@gmail.com>
2022-01-23 16:26:12 -0500

    Update main.cpp

--- PowerAux/src/main.cpp
+++ PowerAux/src/main.cpp
@@ -10,19 +10,58 @@
 
 #define MAIN_LOOP_PERIOD 1s
 #define CAN_PERIOD 1s
+#define FLASH_PERIOD 1s
 
 PowerAuxCANInterface vehicle_can_interface(MAIN_CAN_RX, MAIN_CAN_TX, MAIN_CAN_STBY);
 PowerAuxBPSCANInterface bps_can_interface(BMS_CAN1_RX, BMS_CAN1_TX, BMS_CAN1_STBY);
 
+bool flashHazards, flashLSignal, flashRSignal;
+Thread signalFlashThread;
+
 DigitalOut breakLights(BRAKE_LIGHT_EN);
 DigitalOut headlights(DRL_EN);
 DigitalOut horn(HORN_EN);
-// DigitalOut ignition(IGNITION);
-
-bool hazardsOn, leftSignal, rightSignal;
 DigitalOut leftTurnSignal(LEFT_TURN_EN);
 DigitalOut rightTurnSignal(RIGHT_TURN_EN);
 
+// DigitalOut ignition(IGNITION);
+
+void signalFlashHandler() {
+    if (flashHazards) {
+        if (leftTurnSignal && rightTurnSignal) {
+            leftTurnSignal = false;
+            rightTurnSignal = false;
+        } else {
+            leftTurnSignal = true;
+            rightTurnSignal = true;
+        }
+    } else {
+        leftTurnSignal = false;
+        rightTurnSignal = false;
+    }
+
+    if (flashLSignal) {
+        if (leftTurnSignal) {
+            leftTurnSignal = false;
+        } else {
+            leftTurnSignal = true;
+        }
+    } else {
+        leftTurnSignal = false;
+    }
+
+    if (flashRSignal) {
+        if (rightTurnSignal) {
+            rightTurnSignal = false;
+        } else {
+            rightTurnSignal = true;
+        }
+    } else {
+        rightTurnSignal = false;
+    }
+
+    ThisThread::sleep_for(FLASH_PERIOD);
+}
 
 int main() {
     // device.set_baud(38400);
@@ -31,46 +70,12 @@ int main() {
     PRINT("start main() \r\n");
 #endif //TESTING
 
+    signalFlashThread.start(signalFlashHandler);
+
     while(1) {
         #ifdef TESTING
             PRINT("main thread loop \r\n");
         #endif //TESTING
-
-        PowerAuxExampleStruct a(5, 6, 7, 8);
-        vehicle_can_interface.send(&a);
-
-        if (hazardsOn) {
-            if (leftTurnSignal && rightTurnSignal) {
-                leftTurnSignal = false;
-                rightTurnSignal = false;
-            } else {
-                leftTurnSignal = true;
-                rightTurnSignal = true;
-            }
-        } else {
-            leftTurnSignal = false;
-            rightTurnSignal = false;
-        }
-
-        if (leftSignal) {
-            if (leftTurnSignal) {
-                leftTurnSignal = false;
-            } else {
-                leftTurnSignal = true;
-            }
-        } else {
-            leftTurnSignal = false;
-        }
-
-        if (rightSignal) {
-            if (rightTurnSignal) {
-                rightTurnSignal = false;
-            } else {
-                rightTurnSignal = true;
-            }
-        } else {
-            rightTurnSignal = false;
-        }
 
         ThisThread::sleep_for(MAIN_LOOP_PERIOD);
     }
@@ -78,23 +83,16 @@ int main() {
 
 void PowerAuxCANInterface::handle(ECUPowerAuxCommands *can_struct)
 {
-    /*
-    For brake_lights, headlights, horn, ignition, and battery_contact, the appropriate pin 
-    should be set based on each field. For example, the HORN_EN pin should be set based on \
-    the horn field of the ECUPowerAuxCommands struct.
-    */
+    breakLights = can_struct->brake_lights;
+    headlights = can_struct->headlights;
+    horn = can_struct->horn;
 
-   breakLights = can_struct->brake_lights;
-   headlights = can_struct->headlights;
-   horn = can_struct->horn;
+    flashLSignal = can_struct->left_turn_signal;
+    flashRSignal = can_struct->right_turn_signal;
+    flashHazards = can_struct->hazards;
 
-   leftSignal = can_struct->left_turn_signal;
-   rightSignal = can_struct->right_turn_signal;
-   hazardsOn = can_struct->hazards;
-
-//    igntion = can_struct->ignition;
-//    battery__contact = can_struct->battery_contact;
-
+    //    igntion = can_struct->ignition;
+    //    battery__contact = can_struct->battery_contact;
 }
 
 void PowerAuxBPSCANInterface::handle(PackInformation *can_struct)
