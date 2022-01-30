80956b244034710e352fa8d4e04b0f63edf9ed82
Edward Noe <eln4jtt@virginia.edu>
2022-01-30 16:57:26 -0500

    Implement SolarInputReader::readTotalCurrent()

--- Solar/lib/SolarInputReader/SolarInputReader.cpp
+++ Solar/lib/SolarInputReader/SolarInputReader.cpp
@@ -1,1 +1,16 @@
 #include "SolarInputReader.h"
+
+#define ADC_REF_VOLTAGE 3.3
+
+float get_voltage_from_adc_channel(MCP3008 adc_interface, int channel) {
+    uint16_t raw_value = adc_interface.read_input_u16(channel);
+    return ((float) raw_value) / 1024.0 * ADC_REF_VOLTAGE;
+}
+
+SolarInputReader::SolarInputReader(MCP3008 adc1_interface, MCP3008 adc2_interface) : adc1_interface(adc1_interface), adc2_interface(adc2_interface) {}
+
+uint16_t SolarInputReader::readTotalCurrent() {
+    float voltage = get_voltage_from_adc_channel(adc1_interface, 0);
+    float transformed_value = voltage * 600.0;
+    return (uint16_t)transformed_value;
+}
--- Solar/lib/SolarInputReader/SolarInputReader.h
+++ Solar/lib/SolarInputReader/SolarInputReader.h
@@ -6,29 +6,29 @@
 #include <mbed.h>
 
 class SolarInputReader {
-    public:
-        SolarInputReader(MCP3008 spi1_interface, MCP3008 spi2_interface);
+public:
+    SolarInputReader(MCP3008 adc1_interface, MCP3008 adc2_interface);
 
-        uint16_t readTotalCurrent();
+    uint16_t readTotalCurrent();
 
+    uint16_t readPanel1Voltage();
+    uint16_t readPanel2Voltage();
+    uint16_t readPanel3Voltage();
+    uint16_t readPanel4Voltage();
 
-        uint16_t readPanel1Voltage();
-        uint16_t readPanel2Voltage();
-        uint16_t readPanel3Voltage();
-        uint16_t readPanel4Voltage();
+    uint16_t readPanel1Temp();
+    uint16_t readPanel2Temp();
+    uint16_t readPanel3Temp();
+    uint16_t readPanel4Temp();
 
-        uint16_t readPanel1Temp();
-        uint16_t readPanel2Temp();
-        uint16_t readPanel3Temp();
-        uint16_t readPanel4Temp();
+    uint16_t readPanel1Photo();
+    uint16_t readPanel2Photo();
+    uint16_t readPanel3Photo();
+    uint16_t readPanel4Photo();
 
-        uint16_t readPanel1Photo();
-        uint16_t readPanel2Photo();
-        uint16_t readPanel3Photo();
-        uint16_t readPanel4Photo();
-
-        
-
+private:
+    MCP3008 adc1_interface;
+    MCP3008 adc2_interface; 
 };
 
 #endif
