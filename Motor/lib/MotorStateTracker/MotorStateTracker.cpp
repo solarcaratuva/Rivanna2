b89ed1fb00b22bf8f9b7b938f16ab93f5b1de708
John Chrosniak <jlc9wr@virginia.edu>
2022-01-30 17:03:55 -0500

    Added functions to MotorStateTracker

--- Motor/lib/MotorStateTracker/MotorStateTracker.cpp
+++ Motor/lib/MotorStateTracker/MotorStateTracker.cpp
@@ -2,30 +2,30 @@
 
 void MotorStateTracker::setFrame0(Frame0 new_frame)
 {
-    frame0=new_frame;
+    frame0 = new_frame;
 }
 
 void MotorStateTracker::setFrame1(Frame1 new_frame)
 {
-    frame1=new_frame;
+    frame1 = new_frame;
 }
 
 void MotorStateTracker::setFrame2(Frame2 new_frame)
-{   frame2=new_frame;
+{   frame2 = new_frame;
 
 }
 
-Frame0 getFrame0(void)
+Frame0 MotorStateTracker::getFrame0(void)
 {
     return frame0;
 }
 
-Frame1 getFrame1(void)
+Frame1 MotorStateTracker::getFrame1(void)
 {
     return frame1;
 }
 
-Frame2 getFrame2(void)
+Frame2 MotorStateTracker::getFrame2(void)
 {
     return frame2;
 }
