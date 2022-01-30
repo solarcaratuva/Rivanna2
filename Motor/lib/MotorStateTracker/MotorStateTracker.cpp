55ae15b6a98155ad14fec1f29a64f697b3f091bb
John Chrosniak <jlc9wr@virginia.edu>
2022-01-30 16:31:54 -0500

    Added motor state tracker for storing motor controller can messages

---
+++ Motor/lib/MotorStateTracker/MotorStateTracker.cpp
@@ -0,0 +1,31 @@
+#include "MotorStateTracker.h"
+
+void MotorStateTracker::setFrame0(Frame0 new_frame)
+{
+
+}
+
+void MotorStateTracker::setFrame1(Frame1 new_frame)
+{
+
+}
+
+void MotorStateTracker::setFrame2(Frame2 new_frame)
+{
+
+}
+
+Frame0 getFrame0(void)
+{
+
+}
+
+Frame1 getFrame1(void)
+{
+
+}
+
+Frame2 getFrame2(void)
+{
+    
+}
