80349b219e1673dab975b1a5b4119366028427b2
RobelwUVA <77996550+RobelwUVA@users.noreply.github.com>
2022-02-13 16:51:01 -0500

    PowerAuxErrorStruct changes

--- Common/lib/CANStructs/PowerAuxCANStructs.h
+++ Common/lib/CANStructs/PowerAuxCANStructs.h
@@ -6,20 +6,30 @@
 
 typedef struct PowerAuxErrorStruct : CANStruct
 {
-    uint32_t a;
-    uint16_t b;
-    uint8_t c;
-    int8_t d;
+    bool fan_error;
+    bool brake_light_error;
+    bool headlight_error;
+    bool bms_strobe_error;
+    bool left_turn_error;
+    bool right_turn_error; 
 
     PowerAuxErrorStruct() {}
-    PowerAuxErrorStruct(uint32_t a, uint16_t b, uint8_t c, int8_t d)
-        : a(a), b(b), c(c), d(d) {}
+    // PowerAuxErrorStruct(uint32_t a, uint16_t b, uint8_t c, int8_t d)
+    //     : a(a), b(b), c(c), d(d) {}
 
-    SERIALIZATION_METHODS((a, 32), (b, 16), (c, 8), (d, 8))
+    SERIALIZATION_METHODS(
+        (fan_error, 1), 
+        (brake_light_error, 1), 
+        (headlight_error, 1), 
+        (bms_strobe_error, 1),
+        (left_turn_error, 1),
+        (right_turn_error, 1)
+    
+    );
 
     uint32_t get_message_ID()
     {
-        return PowerAuxExampleStruct_MESSAGE_ID;
+        return PowerAuxErrorStruct_MESSAGE_ID;
     }
 } PowerAuxErrorStruct;
 
