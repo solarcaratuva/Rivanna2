5d3df5ccca013fff69ceed415b7025d311c2d914
GitHub Action <actions@github.com>
2022-03-02 01:51:43 +0000

    Apply clang-format changes

--- Common/lib/CANStructs/CANStructs.h
+++ Common/lib/CANStructs/CANStructs.h
@@ -2,9 +2,9 @@
 #define CAN_STRUCTS_H
 
 #include "ECUCANStructs.h"
-#include "PowerAuxCANStructs.h"
-#include "SolarCANStructs.h"
 #include "MotorControllerCANStructs.h"
 #include "PowerAuxBPSCANStructs.h"
+#include "PowerAuxCANStructs.h"
+#include "SolarCANStructs.h"
 
 #endif
