0da9c45fc877bc3c136082e8679b48b6d1f730be
GitHub Action <actions@github.com>
2022-05-01 01:46:57 +0000

    Apply clang-format changes

--- Motor/lib/MotorControllerCANInterface/MotorControllerCANInterface.h
+++ Motor/lib/MotorControllerCANInterface/MotorControllerCANInterface.h
@@ -7,7 +7,8 @@
 class MotorControllerCANInterface : public CANInterface {
   public:
     MotorControllerCANInterface(PinName rd, PinName td, PinName standby_pin);
-    void request_frames(bool power_status_frame, bool drive_status_frame, bool errors_frame);
+    void request_frames(bool power_status_frame, bool drive_status_frame,
+                        bool errors_frame);
     void handle(MotorControllerPowerStatus *can_struct);
     void handle(MotorControllerDriveStatus *can_struct);
     void handle(MotorControllerError *can_struct);
