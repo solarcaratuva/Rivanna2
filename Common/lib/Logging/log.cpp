bde1c6cf99292d8779f2aec5c455b2782fa1381f
Edward Noe <eln4jtt@virginia.edu>
2022-05-24 21:59:17 -0400

    Add Mbed Mutex to log.h Converted log.c to log.cpp and removed unused features.

--- Common/lib/Logging/log.cpp
+++ Common/lib/Logging/log.cpp
@@ -21,21 +21,13 @@
  */
 
 #include "log.h"
-
-#define MAX_CALLBACKS 32
-
-typedef struct {
-  log_LogFn fn;
-  void *udata;
-  int level;
-} Callback;
+#include <mbed.h>
 
 static struct {
-  void *udata;
-  log_LockFn lock;
+  FILE *udata;
+  Mutex mutex;
   int level;
   bool quiet;
-  Callback callbacks[MAX_CALLBACKS];
 } L;
 
 
@@ -69,36 +61,8 @@ static void stdout_callback(log_Event *ev) {
 }
 
 
-static void file_callback(log_Event *ev) {
-  char buf[64];
-  buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", ev->time)] = '\0';
-  fprintf(
-    ev->udata, "%s %-5s %s:%d: ",
-    buf, level_strings[ev->level], ev->file, ev->line);
-  vfprintf(ev->udata, ev->fmt, ev->ap);
-  fprintf(ev->udata, "\n");
-  fflush(ev->udata);
-}
-
-
-static void lock(void)   {
-  if (L.lock) { L.lock(true, L.udata); }
-}
-
-
-static void unlock(void) {
-  if (L.lock) { L.lock(false, L.udata); }
-}
-
-
 const char* log_level_string(int level) {
   return level_strings[level];
-}
-
-
-void log_set_lock(log_LockFn fn, void *udata) {
-  L.lock = fn;
-  L.udata = udata;
 }
 
 
@@ -112,23 +76,7 @@ void log_set_quiet(bool enable) {
 }
 
 
-int log_add_callback(log_LogFn fn, void *udata, int level) {
-  for (int i = 0; i < MAX_CALLBACKS; i++) {
-    if (!L.callbacks[i].fn) {
-      L.callbacks[i] = (Callback) { fn, udata, level };
-      return 0;
-    }
-  }
-  return -1;
-}
-
-
-int log_add_fp(FILE *fp, int level) {
-  return log_add_callback(file_callback, fp, level);
-}
-
-
-static void init_event(log_Event *ev, void *udata) {
+static void init_event(log_Event *ev, FILE *udata) {
   if (!ev->time) {
     time_t t = time(NULL);
     ev->time = localtime(&t);
@@ -145,7 +93,7 @@ void log_log(int level, const char *file, int line, const char *fmt, ...) {
     .level = level,
   };
 
-  lock();
+  L.mutex.lock();
 
   if (!L.quiet && level >= L.level) {
     init_event(&ev, stderr);
@@ -154,15 +102,5 @@ void log_log(int level, const char *file, int line, const char *fmt, ...) {
     va_end(ev.ap);
   }
 
-  for (int i = 0; i < MAX_CALLBACKS && L.callbacks[i].fn; i++) {
-    Callback *cb = &L.callbacks[i];
-    if (level >= cb->level) {
-      init_event(&ev, cb->udata);
-      va_start(ev.ap, fmt);
-      cb->fn(&ev);
-      va_end(ev.ap);
-    }
-  }
-
-  unlock();
+  L.mutex.unlock();
 }
--- Common/lib/Logging/log.h
+++ Common/lib/Logging/log.h
@@ -13,10 +13,6 @@
 #include <stdbool.h>
 #include <time.h>
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
 #define LOG_VERSION "0.1.0"
 
 typedef struct {
@@ -24,13 +20,13 @@ typedef struct {
   const char *fmt;
   const char *file;
   struct tm *time;
-  void *udata;
+  FILE *udata;
   int line;
   int level;
 } log_Event;
 
 typedef void (*log_LogFn)(log_Event *ev);
-typedef void (*log_LockFn)(bool lock, void *udata);
+typedef void (*log_LockFn)(bool lock, FILE *udata);
 
 enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
@@ -43,16 +39,9 @@ enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 #define log_level(level, ...) log_log(level, __FILE__, __LINE__, __VA_ARGS__)
 
 const char* log_level_string(int level);
-void log_set_lock(log_LockFn fn, void *udata);
 void log_set_level(int level);
 void log_set_quiet(bool enable);
-int log_add_callback(log_LogFn fn, void *udata, int level);
-int log_add_fp(FILE *fp, int level);
 
 void log_log(int level, const char *file, int line, const char *fmt, ...);
-
-#if defined(__cplusplus)
-}
-#endif
 
 #endif
