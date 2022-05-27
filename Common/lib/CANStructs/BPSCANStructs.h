8c2639a8f28668d9398f4806db44f263bcf332e9
Edward Noe <eln4jtt@virginia.edu>
2022-05-26 22:58:52 -0400

    Update CAN message log statement text

--- Common/lib/CANStructs/BPSCANStructs.h
+++ Common/lib/CANStructs/BPSCANStructs.h
@@ -27,11 +27,7 @@ typedef struct BPSPackInformation : CANStruct, bps_bps_pack_information_t {
     void log(int level) {
         log_at_level(
             level,
-            "BPSPackInformation\n pack_voltage: %u\n pack_current:  %u\n "
-            "pack_soc: %u\n"
-            "discharge_relay: %d\n charge_relay: %d",
-            pack_voltage, pack_current, pack_soc, discharge_relay,
-            charge_relay);
+            "BPSPackInformation: pack_voltage %u, pack_current %u, pack_soc %u, discharge_relay %u, charge_relay %u", pack_voltage, pack_current, pack_soc, discharge_relay, charge_relay);
     }
 } BPSPackInformation;
 
@@ -69,27 +65,7 @@ typedef struct BPSError : CANStruct, bps_bps_error_t {
     void log(int level) {
         log_at_level(
             level,
-            "BPSError\n internal_communications_fault: %d\n "
-            "internal_conversion_fault: %d\n weak_cell_fault: %d\n "
-            "low_cell_voltage_fault: %d\n open_wiring_fault: %d\n "
-            "current_sensor_fault: %d\n pack_voltage_sensor_fault: %d\n "
-            "weak_pack_fault: %d\n voltage_redundancy_fault: %d\n "
-            "fan_monitor_fault: %d\n thermistor_fault: %d\n "
-            "canbus_communications_fault: %d\n always_on_supply_fault: %d\n "
-            "high_voltage_isolation_fault: %d\n power_supply_12v_fault: %d\n "
-            "charge_limit_enforcement_fault: %d\n "
-            "discharge_limit_enforcement_fault:  %d\n "
-            "charger_safety_relay_fault:  %d\n internal_memory_fault: %d\n "
-            "internal_thermistor_fault: %d/n internal_logic_fault: %d",
-            internal_communications_fault, internal_conversion_fault,
-            weak_cell_fault, low_cell_voltage_fault, open_wiring_fault,
-            current_sensor_fault, pack_voltage_sensor_fault, weak_pack_fault,
-            voltage_redundancy_fault, fan_monitor_fault, thermistor_fault,
-            canbus_communications_fault, always_on_supply_fault,
-            high_voltage_isolation_fault, power_supply_12v_fault,
-            charge_limit_enforcement_fault, discharge_limit_enforcement_fault,
-            charger_safety_relay_fault, internal_memory_fault,
-            internal_thermistor_fault, internal_logic_fault);
+            "BPSError: internal_communications_fault %u, internal_conversion_fault %u, weak_cell_fault %u, low_cell_voltage_fault %u, open_wiring_fault %u, current_sensor_fault %u, pack_voltage_sensor_fault %u, weak_pack_fault %u, voltage_redundancy_fault %u, fan_monitor_fault %u, thermistor_fault %u, CANBUS_communications_fault %u, always_on_supply_fault %u, high_voltage_isolation_fault %u, power_supply_12v_fault %u, charge_limit_enforcement_fault %u, discharge_limit_enforcement_fault %u, charger_safety_relay_fault %u, internal_memory_fault %u, internal_thermistor_fault %u, internal_logic_fault %u", internal_communications_fault, internal_conversion_fault, weak_cell_fault, low_cell_voltage_fault, open_wiring_fault, current_sensor_fault, pack_voltage_sensor_fault, weak_pack_fault, voltage_redundancy_fault, fan_monitor_fault, thermistor_fault, canbus_communications_fault, always_on_supply_fault, high_voltage_isolation_fault, power_supply_12v_fault, charge_limit_enforcement_fault, discharge_limit_enforcement_fault, charger_safety_relay_fault, internal_memory_fault, internal_thermistor_fault, internal_logic_fault);
     }
 } BPSError;
 
@@ -115,10 +91,7 @@ typedef struct BPSCellVoltage : CANStruct, bps_bps_cell_voltage_t {
     void log(int level) {
         log_at_level(
             level,
-            "BPSCellVoltage\n low_cell_voltage: %u\n low_cell_voltage_id: %u\n "
-            "high_cell_voltage: %u\n high_cell_voltage_id: %u",
-            low_cell_voltage, low_cell_voltage_id, high_cell_voltage,
-            high_cell_voltage_id);
+            "BPSCellVoltage: low_cell_voltage %u, low_cell_voltage_id %u, high_cell_voltage %u, high_cell_voltage_id %u", low_cell_voltage, low_cell_voltage_id, high_cell_voltage, high_cell_voltage_id);
     }
 } BPSCellVoltage;
 
@@ -144,10 +117,7 @@ typedef struct BPSCellTemperature : CANStruct, bps_bps_cell_temperature_t {
     void log(int level) {
         log_at_level(
             level,
-            "BPSCellTemperature: \n low_temperature: %u\n low_thermistor_id: "
-            "%u\n high_temperature: %u\n high_thermistor_id: %u\n",
-            low_temperature, low_thermistor_id, high_temperature,
-            high_thermistor_id);
+            "BPSCellTemperature: low_temperature %u, low_thermistor_id %u, high_temperature %u, high_thermistor_id %u", low_temperature, low_thermistor_id, high_temperature, high_thermistor_id);
     }
 } BPSCellTemperature;
 
