7990ecc3063e275bae721df64f7ea81701cb3d44
Edward Noe <eln4jtt@virginia.edu>
2022-05-30 16:46:43 -0400

    Add timeout for ECUMotorCommands in Motor If the CAN bus fails for some reason, we want to set the throttle to 0.

--- Motor/src/main.cpp
+++ Motor/src/main.cpp
@@ -14,6 +14,9 @@
 
 BufferedSerial device(USBTX, USBRX);
 
+EventQueue event_queue(32 * EVENTS_EVENT_SIZE);
+Thread event_thread;
+
 MotorCANInterface vehicle_can_interface(MAIN_CAN_RX, MAIN_CAN_TX);
 MotorControllerCANInterface motor_controller_can_interface(MTR_CTRL_CAN_RX,
                                                            MTR_CTRL_CAN_TX,
@@ -34,6 +37,8 @@ int main() {
     log_set_level(LOG_LEVEL);
     log_debug("Start main()");
 
+    event_thread.start(callback(&event_queue, &EventQueue::dispatch_forever));
+
     while (true) {
         check_motor_board();
         log_debug("Main thread loop");
@@ -44,8 +49,22 @@ int main() {
     }
 }
 
+Timeout ECUMotorCommands_timeout;
+
+// If we have not received an ECUMotorCommands struct in 100ms, we assume that
+// the CAN bus is down and set the throttle to 0.
+void handle_ECUMotorCommands_timeout() {
+    motor_interface.sendThrottle(0x000);
+}
+
 void MotorCANInterface::handle(ECUMotorCommands *can_struct) {
+    // Reset current timeout
+    ECUMotorCommands_timeout.detach();
+    // Set new timeout for 100ms from now
+    ECUMotorCommands_timeout.attach(event_queue.event(handle_ECUMotorCommands_timeout), 100ms);
+
     can_struct->log(LOG_INFO);
+
     motor_interface.sendIgnition(can_struct->motor_on);
     motor_interface.sendDirection(
         can_struct->forward_en); // TODO: verify motor controller will not allow
@@ -57,6 +76,7 @@ void MotorCANInterface::handle(ECUMotorCommands *can_struct) {
 void MotorControllerCANInterface::handle(
     MotorControllerPowerStatus *can_struct) {
     can_struct->log(LOG_INFO);
+
     vehicle_can_interface.send(can_struct);
     motor_state_tracker.setMotorControllerPowerStatus(*can_struct);
 }
@@ -64,12 +84,14 @@ void MotorControllerCANInterface::handle(
 void MotorControllerCANInterface::handle(
     MotorControllerDriveStatus *can_struct) {
     can_struct->log(LOG_INFO);
+
     vehicle_can_interface.send(can_struct);
     motor_state_tracker.setMotorControllerDriveStatus(*can_struct);
 }
 
 void MotorControllerCANInterface::handle(MotorControllerError *can_struct) {
     can_struct->log(LOG_INFO);
+
     vehicle_can_interface.send(can_struct);
     motor_state_tracker.setMotorControllerError(*can_struct);
 }
