a4b85e9636bf5ca4475790b12db499210d6b02e5
John Chrosniak <jlc9wr@virginia.edu>
2022-06-01 16:19:11 -0400

    fixed documentation

--- Motor/lib/MotorInterface/MotorInterface.cpp
+++ Motor/lib/MotorInterface/MotorInterface.cpp
@@ -26,7 +26,7 @@ int MotorInterface::sendThrottle(uint16_t throttle) {
 
 /**
  * Sends regen value to digital POT
- * @param regen Regen value [0, 255]
+ * @param regen Regen value [0, 256]
  */
 int MotorInterface::sendRegen(uint16_t regen) {
     uint16_t updated_regen = 0x100 - regen;
