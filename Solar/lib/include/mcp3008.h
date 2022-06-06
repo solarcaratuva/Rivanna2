eec89fe2f909de99b6408dbc1da39a249697664e
Edward Noe <eln4jtt@virginia.edu>
2022-06-06 11:06:28 -0400

    Revert "Apply clang-format changes"
    This reverts commit bb1191936c0ab5e1091d06b34f4b11081385fe58.

--- Solar/lib/include/mcp3008.h
+++ Solar/lib/include/mcp3008.h
@@ -1,32 +1,39 @@
 /**
  * @file mcp32008.h
  */
-
+ 
 #include "mbed.h"
-
+ 
+ 
 #ifndef MCP3008_H
 #define MCP3008_H
-
+ 
+ 
 /** Polarity setting for differential inputs.
  *
  * POL_EVEN_POSITIVE sets channel [0|2|4|6] as the positive side and channel
  * [1|3|5|7] as the negative side. POL_EVEN_NEGATIVE sets the opposite.
  */
-enum Polarity { POL_EVEN_POSITIVE, POL_EVEN_NEGATIVE };
-
+enum Polarity {
+    POL_EVEN_POSITIVE,
+    POL_EVEN_NEGATIVE
+};
+ 
+ 
 /** Class for interfacing to the MCP3008 SPI-based ADC.
  *
  */
-class MCP3008 {
-  public:
+class MCP3008
+{
+public:
     /** Create an MCP3008 object.
      *
      * @param bus An SPI bus object.
      * @param cs The name of a pin to use as the chip select.
      */
-    MCP3008(SPI *p_bus, PinName cs);
+    MCP3008(SPI* p_bus, PinName cs);
     ~MCP3008();
-
+    
     /** Read from a single-ended input.
      *
      * @param channel The channel number to read from.
@@ -34,16 +41,15 @@ class MCP3008 {
      * @param returns The sampled value as a float between 0.0 and 1.0.
      */
     float read_input(int channel);
-
+    
     /** Read from a single-ended input.
      *
      * @param channel The channel number to read from.
      *
-     * @param returns The sampled value as a unsigned int16_t  between 0 and
-     * 1024.
+     * @param returns The sampled value as a unsigned int16_t  between 0 and 1024.
      */
     uint16_t read_input_u16(int channel);
-
+    
     /** Read from a pair of differential inputs.
      *
      * In differential mode, the channels are referred to as 0 to 3, with
@@ -56,14 +62,15 @@ class MCP3008 {
      *
      * @param returns The sampled value as a float between 0.0 and 1.0.
      */
-    // float read_diff_input(int channel, Polarity polarity);
-
-  private:
+    //float read_diff_input(int channel, Polarity polarity);
+  
+private:
     DigitalOut m_cs;
-    SPI *m_p_bus;
-
+    SPI* m_p_bus;
+    
     void select();
     void deselect();
 };
-
+ 
+ 
 #endif
