fa36fd399b290a547ac98c2269b47f5efdecd955
Michael Fatemi <myfatemi04@gmail.com>
2022-12-04 18:21:11 +0000

    cruise control test

--- ECU/src/main.cpp
+++ ECU/src/main.cpp
@@ -7,6 +7,7 @@
 #include <mbed.h>
 #include <rtos.h>
 #include <math.h>
+#include <chrono>
 
 #define LOG_LEVEL              LOG_DEBUG
 #define MAIN_LOOP_PERIOD       1s
@@ -24,17 +25,35 @@ ECUMotorCommands to_motor;
 ECUPowerAuxCommands to_poweraux;
 
 // Cruise Control Logic
-uint8_t current_speed;
+uint8_t currentSpeed;
 
 // Message Sending Threads
 Thread motor_thread;
 Thread poweraux_thread;
 
+long millis() {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()
+    ).count();
+}
+
+int pid(double p, double i, double d, double setpoint, double input, double& lasterror, double& integral, double dt) {
+	double error = setpoint - input;
+	integral += i * error * dt;
+    double p_ = p * error;
+    double i_ = integral;
+    double d_ = d * (error - lasterror) / dt;
+	lasterror = error;
+	return p_ + i_ + d_;
+}
+
 void motor_message_handler() {
     uint16_t regenRamp = 0;
+    double $p = 5, $i = 3, $d = 3, $integral = 0, $lasterror = 0;
+    double lastTime = millis();
     while (true) {
         // Read motor commands
-        double result = pow(1, 0.4);
+        double currentTime = millis();
 
         // Tesla mode
         uint16_t pedalValue = input_reader.readThrottle();
@@ -43,69 +62,33 @@ void motor_message_handler() {
 
         if (pedalValue <= 50) {
             throttleValue = 0;
-            regenValue = 79.159 * pow(50-pedalValue, 0.3);
+            regenValue = 79.159 * pow(50 - pedalValue, 0.3);
         } else if (pedalValue < 100) {
             throttleValue = 0;
             regenValue = 0;
         } else {
-            throttleValue = -56.27610464*pow(156-(pedalValue-100),0.3) + 256;
+            // pedal from 100 --> 256
+            double targetSpeed = ((pedalValue - 100.0) / 156.0 * 800.0);
+            throttleValue = pid($p, $i, $d, targetSpeed, currentSpeed, $lasterror, $integral, currentTime - lastTime);
+            // throttleValue = -56.27610464 * pow(156 - (pedalValue - 100), 0.3) + 256;
             regenValue = 0;
         }
         to_motor.throttle = throttleValue;
         to_motor.regen = regenValue;
         log_error("R: %d T: %d", regenValue, throttleValue);
-        
-        // Tesla mode with exponential curve
-        /*
-        uint16_t pedalValue = input_reader.readThrottle();
-        uint16_t regenValue;
-        uint16_t throttleValue;
-
-        if (pedalValue < 100) {
-            throttleValue = 0;
-            regenValue = (-0.0256 * pedalValue * pedalValue) + 256;
-            
-        } else if (pedalValue < 156) {
-            throttleValue = 0;
-            regenValue = 0;
-        } else {
-            throttleValue = (40 * (6.3 - pow((100 - (pedalValue - 156)), 0.4) )) + 4;
-            regenValue = 0;
-        }
-        to_motor.throttle = throttleValue;
-        to_motor.regen = regenValue;
-        */        
-
-        // Brake when let go
-        /*
-        to_motor.throttle = input_reader.readThrottle();
-        if (to_motor.throttle == 0) {
-            regenRamp += 10;
-            if (regenRamp > 256) {
-                regenRamp = 256;
-            }
-        } else {
-            regenRamp = 0;
-        }
-        to_motor.regen = regenRamp;
-        */
-
-        // Normal
-        /*
-        to_motor.throttle = input_reader.readThrottle();
-        to_motor.regen = input_reader.readRegen();
-        */
 
         to_motor.forward_en = input_reader.readForwardEn();
         to_motor.reverse_en = input_reader.readReverseEn();
         to_motor.cruise_control_en = input_reader.readCruiseThrottleEn();
-        to_motor.cruise_control_speed = current_speed +
+        to_motor.cruise_control_speed = currentSpeed +
                                         input_reader.readCruiseSpeedUp() -
                                         input_reader.readCruiseSpeedDown();
         to_motor.motor_on = input_reader.readMotorOn();
 
         // Send message
         vehicle_can_interface.send(&to_motor);
+
+        lastTime = currentTime;
 
         // Sleep
         ThisThread::sleep_for(MOTOR_THREAD_PERIOD);
@@ -141,4 +124,8 @@ int main() {
 
         ThisThread::sleep_for(MAIN_LOOP_PERIOD);
     }
+}
+
+void ECUCANInterface::handle(MotorControllerPowerStatus *can_struct) {
+    currentSpeed = can_struct->motor_rpm;
 }
