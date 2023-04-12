efc9a836f01ea05515b861e3816eaef4ddf1ebaa
Adi <adityakak04@gmail.com>
2023-04-12 19:49:55 -0400

    fix pindef

--- Testing/include/pindef.h
+++ Testing/include/pindef.h
@@ -3,38 +3,20 @@
 
 #ifdef TARGET_UVA_SOLAR_CAR
 
-#define BRAKE_LIGHT = PB_13
-#define LEFT_TURN_LIGHT = PA_0
-#define RIGHT_TURN_LIGHT = PA_1
-#define DRO = PB_12
-#define BMS_STROBE = PC_
-
-enum DigitalInputPins {
-    LEFT_TURN = PB0,
-    RIGHT_TURN = PB15,
-    HAZARDS = PC13,
-    BRAKE = PA9,
-    REGEN = PC6,
-    FORWARD = PB1
-};
+#define BRAKE_LIGHT PB_13
+#define LEFT_TURN_LIGHT PA_0
+#define RIGHT_TURN_LIGHT PA_1
+#define DRO PB_12
+#define BMS_STROBE PC_4
 #define LEFT_TURN PB0
 #define RIGHT_TURN PB15
 #define HAZARDS PC13
 #define BRAKE PA9
 #define REGEN PC6
 #define FORWARD PB1
+#define THROTTLE PB_14
 
-enum AnalogInputPins {
-    THROTTLE = 27,
-};
 
-enum DigitalOutputPins {
-    BRAKE_LIGHT,
-    LEFT_TURN_LIGHT,
-    RIGHT_TURN_LIGHT,
-    DRO,
-    BMS_STROBE
-};
 
 #endif
 
