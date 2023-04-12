7f3ae1672252411596a0082e0d2b9c750ffb7f14
Michael Fatemi <myfatemi04@gmail.com>
2023-04-12 23:40:52 +0000

    new pindef

---
+++ Testing/include/pindef.h
@@ -0,0 +1,20 @@
+enum DigitalInputPins {
+    LEFT_TURN = PB0,
+    RIGHT_TURN = PB15,
+    HAZARDS = PC13,
+    BRAKE = PA9,
+    REGEN = PC6,
+    FORWARD = PB1
+};
+
+enum AnalogInputPins {
+    THROTTLE = 27,
+};
+
+enum DigitalOutputPins {
+    BRAKE_LIGHT = 26,
+    LEFT_TURN_LIGHT = 8,
+    RIGHT_TURN_LIGHT = 9,
+    DRO = 25,
+    BMS_STROBE = 16
+};
