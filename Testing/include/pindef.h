6224934e96464e50b2865cb16fcb11f0c36c7344
Michael Fatemi <myfatemi04@gmail.com>
2023-04-12 23:50:33 +0000

    remove underscores

--- Testing/include/pindef.h
+++ Testing/include/pindef.h
@@ -3,18 +3,18 @@
 
 #ifdef TARGET_UVA_SOLAR_CAR
 
-#define BRAKE_LIGHT PB_13
-#define LEFT_TURN_LIGHT PA_0
-#define RIGHT_TURN_LIGHT PA_1
-#define DRO PB_12
-#define BMS_STROBE PC_4
+#define BRAKE_LIGHT PB13
+#define LEFT_TURN_LIGHT PA0
+#define RIGHT_TURN_LIGHT PA1
+#define DRO PB12
+#define BMS_STROBE PC4
 #define LEFT_TURN PB0
 #define RIGHT_TURN PB15
 #define HAZARDS PC13
 #define BRAKE PA9
 #define REGEN PC6
 #define FORWARD PB1
-#define THROTTLE PB_14
+#define THROTTLE PB14
 
 
 
