bb405c057e709d2623864161729e60d0ab93dd7d
Adi <adityakak04@gmail.com>
2023-04-12 19:46:10 -0400

    pindef for digital output

--- Testing/include/pindef.h
+++ Testing/include/pindef.h
@@ -1,3 +1,14 @@
+#ifndef PINDEF_H
+#define PINDEF_H
+
+#ifdef TARGET_UVA_SOLAR_CAR
+
+#define BRAKE_LIGHT = PB_13
+#define LEFT_TURN_LIGHT = PA_0
+#define RIGHT_TURN_LIGHT = PA_1
+#define DRO = PB_12
+#define BMS_STROBE = PC_
+
 enum DigitalInputPins {
     LEFT_TURN = PB0,
     RIGHT_TURN = PB15,
@@ -12,9 +23,13 @@ enum AnalogInputPins {
 };
 
 enum DigitalOutputPins {
-    BRAKE_LIGHT = 26,
-    LEFT_TURN_LIGHT = 8,
-    RIGHT_TURN_LIGHT = 9,
-    DRO = 25,
-    BMS_STROBE = 16
+    BRAKE_LIGHT,
+    LEFT_TURN_LIGHT,
+    RIGHT_TURN_LIGHT,
+    DRO,
+    BMS_STROBE
 };
+
+#endif
+
+#endif // PINDEF_H
