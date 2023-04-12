7ff23ba6b2fd3ff16d0efef51f87bc0a19a2a67c
Michael Fatemi <myfatemi04@gmail.com>
2023-04-12 23:33:56 +0000

    Integrate signal flash thread

--- Testing/src/main.cpp
+++ Testing/src/main.cpp
@@ -2,6 +2,10 @@
 #include <mbed.h>
 #include <rtos.h>
 #include <stdio.h>
+#include <chrono>
+#include "ThisThread.h"
+
+#define FLASH_PERIOD 500ms
 
 // DigitalOut brake_lights(BRAKE_LIGHT_EN);
 
@@ -37,12 +41,16 @@ enum DigitalOutputPins {
     BMS_STROBE = 16
 };
 
-bool flashHazards, flashLSignal, flashRSignal = false;
 Thread signalFlashThread;
 
-DigitalOut brake_lights(BRAKE_LIGHT_EN);
-DigitalOut leftTurnSignal(LEFT_TURN_EN);
-DigitalOut rightTurnSignal(RIGHT_TURN_EN);
+DigitalOut brake_lights(BRAKE_LIGHT);
+DigitalOut leftTurnSignal(LEFT_TURN_LIGHT);
+DigitalOut rightTurnSignal(RIGHT_TURN_LIGHT);
+
+// bool flashHazards, flashLSignal, flashRSignal = false;
+DigitalIn flashHazards(HAZARDS);
+DigitalIn flashLSignal(LEFT_TURN);
+DigitalIn flashRSignal(RIGHT_TURN);
 
 const PinName SPI_CS = D0;
 const PinName SPI_MOSI = D0;
@@ -82,79 +90,6 @@ int initializeADC() {
 }
 
 int readADC() {
-
-    return 0;
-}
-
-enum ButtonMasks {
-    THROTTLE = 1 << 0,
-    BRAKE_PEDAL = 1 << 1,
-    REGEN_PEDAL = 1 << 2,
-    LEFT_TURN_SIGNAL = 1 << 3,
-    RIGHT_TURN_SIGNAL = 1 << 4,
-    RUNNING_LIGHTS = 1 << 5,
-    HORN = 1 << 6,
-    HAZARDS = 1 << 7,
-    REVERSE = 1 << 8,
-    FORWARD = 1 << 9,
-    REGEN_ENABLED = 1 << 10,
-    IGNITION = 1 << 11,
-    DRL = 1 << 12,
-    BRAKE_LIGHT = 1 << 13,
-    BMS_STROBE = 1 << 14
-};
-
-int handle_button_state(int buttonState) {
-    if (buttonState & THROTTLE) {
-        // handle throttle
-    }
-    if (buttonState & BRAKE_PEDAL) {
-        // handle brake pedal
-    }
-    if (buttonState & REGEN_PEDAL) {
-        // handle regen pedal
-    }
-    if (buttonState & LEFT_TURN_SIGNAL) {
-        // handle left turn signal
-    }
-    if (buttonState & RIGHT_TURN_SIGNAL) {
-        // handle right turn signal
-    }
-    if (buttonState & RUNNING_LIGHTS) {
-        // handle running lights
-    }
-    if (buttonState & HORN) {
-        // handle horn
-    }
-    if (buttonState & HAZARDS) {
-        // handle hazards
-    }
-    if (buttonState & REVERSE) {
-        // handle reverse
-    }
-    if (buttonState & FORWARD) {
-        // handle forward
-    }
-    if (buttonState & REGEN_ENABLED) {
-        // handle regen enabled
-    }
-    if (buttonState & IGNITION) {
-        // handle ignition
-    }
-    if (buttonState & DRL) {
-        // handle DRL
-    }
-    if (buttonState & BRAKE_LIGHT) {
-        // handle brake light
-    }
-    if (buttonState & BMS_STROBE) {
-        // handle BMS strobe
-    }
-    
-    return 0;
-}
-
-int main() {
     // Configure SPI parameters
     spi.format(16, 3);       // 16-bit data, mode 3
     spi.frequency(1000000);  // 1 MHz clock speed
@@ -168,6 +103,46 @@ int main() {
     cs = 0;
     spi.write(0x8000 | (OSR << 8) | (PGA << 4) | BW);  // Write to configuration register
     cs = 1;
+
+    return 0;
+}
+
+void signalFlashHandler() {
+    while (true) {
+        if (flashHazards || flashLSignal || flashRSignal) {
+            if (flashHazards) {
+                bool leftTurnSignalState = leftTurnSignal;
+                leftTurnSignal = !leftTurnSignalState;
+                rightTurnSignal = !leftTurnSignalState;
+            } else if (flashLSignal) {
+                leftTurnSignal = !leftTurnSignal;
+                rightTurnSignal = false;
+            } else if (flashRSignal) {
+                rightTurnSignal = !rightTurnSignal;
+                leftTurnSignal = false;
+            } else {
+                leftTurnSignal = false;
+                rightTurnSignal = false;
+            }
+
+            ThisThread::sleep_for(FLASH_PERIOD);
+        } else {
+            leftTurnSignal = false;
+            rightTurnSignal = false;
+        }
+        ThisThread::flags_wait_all(0x1);
+    }
+}
+
+int main() {
+    // No SPI for now.
+    signalFlashThread.start(signalFlashHandler);
+
+    while (true) {
+        log_debug("Main thread loop");
+
+        ThisThread::sleep_for(MAIN_LOOP_PERIOD);
+    }
 
     return 0;
 }
